e2c480c765705471878b80bd8a9e0191623580e9
robso <robso@BROBSON>
2022-11-21 22:13:49 -0300

    Adicionar arquivos de projeto.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MyTodoApp.Models;
+
+namespace MyTodoApp.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Todo> Todos { get; set; }
+    //Todos é a tabela.
+
+}
---
+++ Models/Todo.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyTodoApp.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Título")]
+        [Required(ErrorMessage ="Campo Obrigatório")]
+        public string? Title { get; set; }
+
+        [DisplayName("Concuído")]
+        public bool Done { get; set; }
+
+        [DisplayName("Criado em")]
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        [DisplayName("Ultima Atualização")]
+        public DateTime LastUpdateDate { get; set; } = DateTime.Now;
+
+        [DisplayName("Usuário")]
+        public string? User { get; set; }
+
+    }
+}
